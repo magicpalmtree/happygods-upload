993dde0b2b4238f0cbc756eba137505bbc2ab822
codecookerpro <codecookerpro@gmail.com>
2022-09-30 17:45:00 -0500

    Complete uploading nft image

---
+++ src/helpers/constants.ts
@@ -0,0 +1,11 @@
+export const IPFS_PROJECT_ID =
+  process.env.REACT_APP_IPFS_ID || '2DPQZ88vN6DworNjKmEULrEVRuo';
+export const IPFS_PROJECT_SECRET =
+  process.env.REACT_APP_IPFS_KEY || '01493be23ac5ddd53beba8db5294d588';
+
+export const MORALIS_SERVER_URL =
+  process.env.REACT_APP_MORALIS_SERVER_URL ||
+  'https://8h3iz0xd2tq1.usemoralis.com:2053/server';
+export const MORALIS_APP_ID =
+  process.env.REACT_APP_MORALIS_APP_ID ||
+  '4qL9uh7DHr0OOZXruVgr4c87Nq301CnCN7lQ9vi9';
